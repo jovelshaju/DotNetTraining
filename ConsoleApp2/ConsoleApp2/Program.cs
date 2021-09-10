1e409b14bbcb74d309f9aa6bd5ed1d54bd50a226
jovelshaju <Edvel#2011>
2021-09-10 13:00:47 +0530

    Function Added

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            AddNumbers();
+            Console.ReadLine();
+
+        }
+
+        static void AddNumbers()
+        {
+            Console.WriteLine("\nFunction to Add Two Numbers\n===================");
+            Console.WriteLine("Enter first number:");
+            dynamic num1 = Console.ReadLine();
+            Console.WriteLine("Enter second number:");
+            dynamic num2 = Console.ReadLine();
+
+            num1 = int.Parse(num1);
+            num2 = int.Parse(num2);
+            int sum = num1 + num2;
+
+            Console.WriteLine($"{num1} + {num2} = {sum}");
+
         }
     }
 }
